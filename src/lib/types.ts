5f5ca78faa996ae340d8325d6c7aec907409e837
Jose Manuel Madriaza <jose.madriaza@thenotcompany.com>
2023-03-24 19:30:32 -0300

    change tab to spaces

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,13 +1,13 @@
 export type filterType = {
-	[key: string]: (text: string, params?: { [key: string]: number }) => string;
+  [key: string]: (text: string, params?: { [key: string]: number }) => string;
 }
   
 export type optionsType = {
-	[key: string]: {
-		name: string;
-		active: boolean;
-		params: {
-			[key: string]: number;
-		}
-	};
+  [key: string]: {
+    name: string;
+    active: boolean;
+    params: {
+      [key: string]: number;
+    }
+  };
 }
