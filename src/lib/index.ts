d7573c2ce9b65a92c89cb3b733f7262e310e2eb8
Jose Manuel Madriaza <jose.madriaza@thenotcompany.com>
2023-03-24 19:25:11 -0300

    change the use of the library a bit

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,15 +1,17 @@
+import { optionsType, filterType } from "./types";
+
 // Pregunta 1
-const addIndentationToPhrase = (text: string, n: number) => text.replace(/(?<=\. )/g, " ".repeat(n));
+const addIndentationToPhrase = (text: string, { n }: any) => text.replace(/(?<=\. )/g, " ".repeat(n));
 
 // Pregunta 2
-const addLineBreaks = (text: string, n: number) =>
+const addLineBreaks = (text: string, { n }: any) =>
   text
     .split("\n\n")
     .map((paragraph) => paragraph.split("\n").join(" ").trim())
     .join("\n".repeat(n + 1));
 
 // Pregunta 3
-const limitWidth = (text: string, n: number) =>
+const limitWidth = (text: string, { n }: any ) =>
   text
     .split("\n")
     .map((line) => {
@@ -31,10 +33,10 @@ const limitWidth = (text: string, n: number) =>
     .join("\n");
 
 // Pregunta 4
-const addParagraphIndentation = (text: string, n: number) => text.replace(/^(?!\s*$)/gm, " ".repeat(n));
+const addParagraphIndentation = (text: string, { n }: any) => text.replace(/^(?!\s*$)/gm, " ".repeat(n));
 
 // Pregunta 5 y 6
-const filterParagraphs = (text: string, min: number, max: number) =>
+const filterParagraphs = (text: string, { min, max }: any) =>
   text
     .split("\n\n")
     .filter((paragraph) => {
@@ -58,22 +60,82 @@ const convertToParagraphs = (text: string) => {
   return paragraphs.join("");
 };
 
-const takeFirstSentences = (text: string, n: number) =>
+const takeFirstSentences = (text: string, { n }: any) =>
   text
     .split("\n\n")
     .map((paragraph) => paragraph.split(".").slice(0, n).join(".") + ".")
     .join("\n\n");
 
-const defaultOptions = {
-  indentation: 2,
-  lineBreaks: 2,
-  width: 80,
-  paragraphIndentation: 4,
-  minSentences: 4,
-  maxSentences: 10,
-  firstSentences: 2,
+const filters: filterType = {
+  addIndentationToPhrase,
+  addLineBreaks,
+  limitWidth,
+  addParagraphIndentation,
+  filterParagraphs,
+  convertToParagraphs,
+  takeFirstSentences,
 };
 
-export function transform(text: string, options = defaultOptions) {
-  return text.replaceAll("hola", "adios");
+export const defaultOptions: optionsType = {
+  addIndentationToPhrase: {
+    name: "Add indentation to phrase",
+    active: false,
+    params: {
+      n: 2
+    }
+  },
+  addLineBreaks: {
+    name: "Add line breaks",
+    active: false,
+    params: {
+      n: 2
+    }
+  },
+  limitWidth: {
+    name: "Limit width",
+    active: false,
+    params: {
+      n: 80
+    }
+  },
+  addParagraphIndentation: {
+    name: "Add paragraph indentation",
+    active: false,
+    params: {
+      n: 4
+    }
+  },
+  filterParagraphs: {
+    name: "Filter paragraphs",
+    active: false,
+    params: {
+      min: 4,
+      max: 10
+    }
+  },
+  convertToParagraphs: {
+    name: "Convert to paragraphs",
+    active: false,
+    params: {}
+  },
+}
+
+/**
+ * Function that applies the filters to the text according to the options.
+ * When a filter is active, it is applied to the text, with its corresponding
+ * parameters, set in the params property of the filter.
+ * 
+ * @param text - The text to be transformed.
+ * @param options - Determine which filters are active and their parameters.
+ * 
+ * @returns {string} The transformed text.
+ */
+export function transform(text: string, options: optionsType = defaultOptions): string {
+  let transformedText = text
+  Object.entries(options).forEach(([filterName, filterConfig]) => {
+    if (filterConfig.active) {
+      transformedText = filters[filterName](transformedText, options[filterName].params)
+    }
+  })
+  return transformedText
 }
