2a07153931d6df7f5dc0d530c25d4ca0000ffaf6
Jose Manuel Madriaza <jose.madriaza@thenotcompany.com>
2023-03-24 19:53:48 -0300

    fix new text bug

--- src/demo/main.tsx
+++ src/demo/main.tsx
@@ -3,23 +3,23 @@ import "./style.css";
 import exampleText from "../example/input.txt?raw";
 
 import { useState, useCallback } from "react";
-import { transform } from "../lib";
+import { transform, defaultOptions } from "../lib";
 import { optionsType } from "../lib/types";
 
 import OptionsDialog from "./dialog/OptionsDialog";
 
 export default function App(): JSX.Element {
   const [text, setText] = useState(exampleText);
-  const [transformedText, setTransformedText] = useState(transform(text));
+  const [options, setOptions] = useState(defaultOptions)
 
   /**
    * Callback function for when the options are saved.
-   * Executes the transform function with the new options.
+   * Save the new options in the state.
    * 
    * @param options - The new options to use in the transform function.
    */
   const onSave = useCallback((options: optionsType) => {
-    setTransformedText(transform(text, options));
+    setOptions((prev) => ({ ...prev, ...options }));
   }, []);
 
   return (
@@ -31,7 +31,7 @@ export default function App(): JSX.Element {
       </div>
       <div className="grid grid-cols-2 min-h-full">
         <textarea className="p-2" value={text} onChange={(e) => setText(e.target.value)} />
-        <textarea className="p-2" value={transformedText} disabled />
+        <textarea className="p-2" value={transform(text, options)} disabled />
       </div>
     </>
   );
