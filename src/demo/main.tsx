008758afd8ba62e04c8105e114b7930c1c849e82
Benjamín Vicente <benjavicente@uc.cl>
2023-03-26 19:37:21 -0300

    css stuff

--- src/demo/main.tsx
+++ src/demo/main.tsx
@@ -15,11 +15,18 @@ export default function App(): JSX.Element {
 
   return (
     <>
-      <div className="container py-4 px-10 mx-0 min-w-full flex flex-col items-center">
+      <div className="container py-4 mx-0 min-w-full flex flex-col items-center bg-gray-100 border-b">
         <h1 className="text-4xl font-bold text-center text-indigo-600 mb-3"> Functional JS</h1>
-        <OptionsDialog options={options} setOptions={setOptions} />
+        <OptionsDialog
+          options={options}
+          setOptions={setOptions}
+          reset={() => {
+            setText("");
+            setOptions([]);
+          }}
+        />
       </div>
-      <div className="grid grid-cols-2 min-h-full">
+      <div className="grid grid-cols-2 flex-grow text-sm sm:text-base">
         <textarea className="p-2" value={text} onChange={(e) => setText(e.target.value)} />
         <textarea className="p-2" value={transform(text, options)} disabled />
       </div>
